78b4876393e06a8eceb3ad07e1ff9f3203cebba7
HussainUW <hkuvawal@uwaterloo.ca>
2021-12-02 15:06:18 -0500

    completing edit task and delete task functionality...

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -1,9 +1,19 @@
 import React, {Fragment, useEffect, useState} from "react";
+import EditTodo from "./EditTodo";
 
 const ListTodos = () => {
     
     const [todos, setTodos] = useState([]);
-    
+
+    const deleteTodo = async (id) => {
+        try {
+            const deleteTodo = await fetch(`http://localhost:5000/todos/${id}`, {method: "DELETE"});
+            setTodos(todos.filter(todo => todo.todo_id !== id)); 
+        } catch (error) {
+            console.error(error.message);
+        }
+    };
+
     const getTodos = async () => {
         try {
             const response = await fetch("http://localhost:5000/todos");
@@ -31,10 +41,14 @@ const ListTodos = () => {
         <tbody>
 
         {todos.map(todo => (
-            <tr>
+            <tr key={todo.todo_id}>
                 <td>{todo.description}</td>
-                <td>Edit</td>
-                <td>Delete</td>
+                <td><EditTodo todo = {todo} /> </td>
+                <td>
+                    <button className = "btn btn-danger" onClick={()=> deleteTodo(todo.todo_id)}>
+                        Delete
+                    </button>
+                </td>
             </tr>
         ))}
         </tbody>
