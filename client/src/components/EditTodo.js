ee378d77debfc163b50106411c04f4d5a33d2969
HussainUW <hkuvawal@uwaterloo.ca>
2021-12-02 17:07:40 -0500

    minor changes

--- client/src/components/EditTodo.js
+++ client/src/components/EditTodo.js
@@ -14,6 +14,7 @@ const EditTodo = ({todo}) => {
                 headers: {"Content-Type": "application/json"},
                 body: JSON.stringify(body)
             });
+            console.log(response);
             window.location = "/";
         } catch (error) {
             console.error(error.message);
@@ -24,7 +25,7 @@ const EditTodo = ({todo}) => {
     return(
         <Fragment>
             
-        <button type="button" class="btn btn-warning" data-toggle="modal" data-target={`#id${todo.todo_id}`}>
+        <button type="button" className="btn btn-warning" data-toggle="modal" data-target={`#id${todo.todo_id}`}>
           Edit
         </button>
         
--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -2,58 +2,86 @@ import React, {Fragment, useEffect, useState} from "react";
 import EditTodo from "./EditTodo";
 
 const ListTodos = () => {
-    
+
     const [todos, setTodos] = useState([]);
 
-    const deleteTodo = async (id) => {
+    //delete todo function
+    const deleteTodo = async id =>{
         try {
             const deleteTodo = await fetch(`http://localhost:5000/todos/${id}`, {method: "DELETE"});
-            setTodos(todos.filter(todo => todo.todo_id !== id)); 
-        } catch (error) {
-            console.error(error.message);
+            setTodos(todos.filter(todo => todo.todo_id !== id));
+            console.log(deleteTodo);
+        } catch (err) {
+            console.error(err.message);
         }
     };
 
-    const getTodos = async () => {
+    //delete all todos
+    const deleteAllTodos = async() => {
         try {
+            const deleteAllTodos = await fetch("http://localhost:5000/todos", {method:"DELETE"});
+            console.log(deleteAllTodos);
+        } catch (error) {
+            console.error(error.message);
+        }
+    }
+
+    const getTodos = async() => {
+        try {
+
             const response = await fetch("http://localhost:5000/todos");
             const jsonData = await response.json();
-            
+
             setTodos(jsonData);
-        } catch (error) {
-            console.error(error.message);
+            
+        } catch (err) {
+            console.error(err.message);
         }
     };
 
-    useEffect(() =>{
+    useEffect(() => {
         getTodos();
     }, []);
 
     return <Fragment>
-    <table className="table table-hover mt-5 text-center">
-        <thead>
-        <tr>
-            <th>Description</th>
-            <th>Edit</th>
-            <th>Delete</th>
-        </tr>
-        </thead>
-        <tbody>
-
-        {todos.map(todo => (
-            <tr key={todo.todo_id}>
-                <td>{todo.description}</td>
-                <td><EditTodo todo = {todo} /> </td>
-                <td>
-                    <button className = "btn btn-danger" onClick={()=> deleteTodo(todo.todo_id)}>
-                        Delete
-                    </button>
-                </td>
-            </tr>
-        ))}
-        </tbody>
-   </table>
-</Fragment>
+        <table className="table mt-5 text-center">
+            <thead>
+                <tr>
+                    <th>Description</th>
+                    <th>Edit</th>
+                    <th>Delete</th>
+                </tr>
+            </thead>
+            <tbody>
+                {todos.map(todo => (
+                    <tr key={todo.todo_id}>
+                        <td>{todo.description}</td>
+                        <td>
+                            <EditTodo todo = {todo}/>
+                        </td>
+                        <td>
+                            <button 
+                            className="btn btn-danger" 
+                            onClick={() => deleteTodo(todo.todo_id)}
+                            >Delete</button>
+                        </td>
+                    </tr>                   
+                ))}
+            </tbody>
+            <tfoot>
+               <tr>
+                   <td></td>
+                   <td></td>
+                   <td>
+                       <button
+                            className="btn btn-danger"
+                            onClick = {() => deleteAllTodos()}
+                       >Delete All</button>
+                   </td>
+               </tr>
+            </tfoot>
+            </table>
+    </Fragment>
 };
 
 export default ListTodos;
