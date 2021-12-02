21c6f4140715893845a49eb1ea6066307e3c2c63
HussainUW <hkuvawal@uwaterloo.ca>
2021-12-02 18:47:24 -0500

    improving syntax for editTodo

--- client/src/components/EditTodo.js
+++ client/src/components/EditTodo.js
@@ -30,32 +30,32 @@ const EditTodo = ({todo}) => {
         </button>
         
         
-        <div class="modal" id = {`id${todo.todo_id}`}
+        <div className="modal" id = {`id${todo.todo_id}`}
         onClick = {() => setDescription(todo.description)} >
-          <div class="modal-dialog">
-            <div class="modal-content">
+          <div className="modal-dialog">
+            <div className="modal-content">
         
         
-              <div class="modal-header">
-                <h4 class="modal-title">Edit Todo</h4>
-                <button type="button" class="close" data-dismiss="modal"
+              <div className="modal-header">
+                <h4 className="modal-title">Edit Todo</h4>
+                <button type="button" className="close" data-dismiss="modal"
                 onClick = {() => setDescription(todo.description)}
                 >&times;</button>
               </div>
         
         
-              <div class="modal-body">
+              <div className="modal-body">
                 <input type = "text" className = "form-control" value = {description} 
                 onChange={e => setDescription(e.target.value)} />
               </div>
         
         
-              <div class="modal-footer">
-                <button type="button" class="btn btn-warning" data-dismiss="modal" 
+              <div className="modal-footer">
+                <button type="button" className="btn btn-warning" data-dismiss="modal" 
                 onClick = {e => updateDescription(e)}>
                     Edit</button>
               
-                <button type="button" class="btn btn-danger" data-dismiss="modal" 
+                <button type="button" className="btn btn-danger" data-dismiss="modal" 
                 onClick = {() => setDescription(todo.description)}>
                     Close</button>
               </div>
